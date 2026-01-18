a8fb5683b0f33e63bcb93d5038ca5c02eb6ad6e2
Itnamezr <itsawatsirii@gmail.com>
2026-01-18 16:32:34 +0700

    Update farming.lua

--- Service/farming.lua
+++ Service/farming.lua
@@ -4,8 +4,8 @@ local Load_Script = function()
 
      local Url_Script "https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"
 
-     getgenv().Key = "MARU-HGINP-N4FN-D9FJ-9OKVN-8A2LK"
-     getgenv().id = "879215493129568346"
+     getgenv().Key = "MARU-P9K5F-DCTY-XMQ6P-PKLG-9XWEY"
+     getgenv().id = "856718169410371614"
      loadstring(game:HttpGet(Url_Script))()
 
 end
